ac02a2a515168fc8efd456df310b6e6625bc1c04
katreyna <kathyareynosa@gmail.com>
2025-11-04 04:48:47 +0000

    fixed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt"); // encryption
 
 
 
-const PORT = 80; 
+const PORT = 8080; 
 const directory  = path.join(__dirname, "public_html");
 
 const connection_pool = mysql.createPool({
